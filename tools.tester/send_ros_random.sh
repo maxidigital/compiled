4afa25d5804ca2fc44fcf9be09aa9d52c355628f
Maxi <maxidigital@gmail.com>
2025-03-07 18:13:45 +0530

    V2XMQTTConverter & DENMWarnungSender

---
+++ tools.tester/send_ros_random.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+RECIPIENT_TYPE=ros    # [cohda, ros, raw]
+OUT_PORT=2009
+
+MESSAGE_ID=14
+PROTOCOL_ID=1
+
+java -jar tools.tester-2.8.jar --out ${RECIPIENT_TYPE}:ind:${OUT_PORT} --random ${MESSAGE_ID}:${PROTOCOL_ID}  --mirror --interval 5000
